2a212ce5070ff67000c9bbcf17e0845c3f57520f
Ubuntu <robert@Discworld.Discworld-js1h704o.g10.internal.cloudapp.net>
2015-10-02 03:30:49 +0000

    website stuff

---
+++ 404.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<title>404 Memes Not Found</title>
+</head>
+<body>
+	<center>
+		<h3>Sorry, we couldn't find that sweet meme for you.</h3>
+		<h4>Maybe this one will work instead.<h4>
+		<a href="http://discworld-js1h704o.cloudapp.net">
+			<img src="pics/404.png" alt="sweetmem">
+		</a>
+	</center>
+</body>
+</html>
